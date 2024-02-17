1d61644fb17939de5e5ad936c41c950313e0a525
Reinaldo G. P. Neto <reinaldogpn@outlook.com>
2024-02-17 17:38:03 -0300

    Update windows-post-install.bat

--- windows-post-install.bat
+++ windows-post-install.bat
@@ -38,11 +38,13 @@ cd %~dp0
 
 :: ------------ VARIÁVEIS ------------ ::
 
-set APP_LIST=%~dp0apps.txt
-set RESOURCES_PATH=%~dp0resources
-set COUNT=0
+set "APP_LIST=%~dp0apps.txt"
+set "RESOURCES_PATH=%~dp0resources"
+set "COUNT=0"
+set "OS_name="
+set "OS_version="
 
-:: ------------ FUNÇÕES ------------ ::
+:: ------------ TESTES ------------ ::
 
 :: Executando como admin?
 
@@ -70,6 +72,52 @@ echo.
 
 :: ------------------------ ::
 
+:: Versão do Windows?
+
+for /f "tokens=*" %%a in ('systeminfo ^| findstr /B /C:"Nome do sistema operacional"') do (
+    set "count=0"
+    for %%b in (%%a) do (
+        set /a count+=1
+        if !count! geq 5 (
+            set "OS_name=!OS_name! %%b"
+        )
+    )
+)
+
+set "OS_name=!OS_name:~1!"
+
+for /f "tokens=7" %%c in ('systeminfo ^| findstr /B /C:"Nome do sistema operacional"') do (
+    set "OS_version=%%c"
+)
+
+echo Sistema operacional identificado: %OS_name%
+
+echo.
+
+:: ------------------------ ::
+
+:: Winget instalado?
+
+echo Verificando a instalação do winget...
+where winget >nul 2>&1 || (
+    echo Instalando o winget e suas dependências...
+    powershell -Command "Add-AppxPackage -Path '%RESOURCES_PATH%\winget\Microsoft.UI.Xaml_7.2208.15002.0_X64_msix_en-US.msix'"
+    powershell -Command "Add-AppxPackage -Path '%RESOURCES_PATH%\winget\Microsoft.VC.2015.UWP.DRP_14.0.30704.0_X64_msix_en-US.msix'"
+    powershell -Command "Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile winget.msixbundle; .\winget.msi"
+    echo y | winget list >nul 2>&1
+    if !errorlevel! equ 0 (
+        echo Atualizando o winget...
+        winget upgrade Microsoft.AppInstaller --accept-package-agreements --accept-source-agreements --disable-interactivity --silent
+        echo Winget está devidamente instalado e atualizado.
+    ) else (
+        echo Ocorreu um erro.
+    )
+)
+
+echo.
+
+:: ------------ FUNÇÕES ------------ ::
+
 :: Ponto de restauração 1
 
 echo Criando ponto de restauração do sistema...
@@ -81,29 +129,22 @@ echo.
 
 :: ------------------------ ::
 
-:: Winget instalado?
+:: Configurações e serviços de rede
 
-echo Verificando a existência do Winget...
-where winget >nul 2>&1 || (
-    echo Instalando o winget e suas dependências...
-    powershell -Command "Add-AppxPackage -Path '%RESOURCES_PATH%\winget\Microsoft.UI.Xaml_7.2208.15002.0_X64_msix_en-US.msix'"
-    powershell -Command "Add-AppxPackage -Path '%RESOURCES_PATH%\winget\Microsoft.VC.2015.UWP.DRP_14.0.30704.0_X64_msix_en-US.msix'"
-    powershell -Command "Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile winget.msixbundle; .\winget.msi"
-    echo y | winget list >nul 2>&1
-    if !errorlevel! equ 0 (
-        echo Winget está devidamente instalado e configurado.
-    ) else (
-        echo Ocorreu um erro.
-    )
-)
+echo Habilitando serviço de FTP...
+powershell -c "dism /online /enable-feature /featurename:IIS-WebServerRole /all"
+powershell -c "dism /online /enable-feature /featurename:IIS-WebServer /all"
+powershell -c "dism /online /enable-feature /featurename:IIS-FTPServer /all"
+netsh advfirewall firewall add rule name="FTP" dir=in action=allow protocol=TCP localport=21
 
-echo.
+echo Habilitando serviço de SSH...
+powershell -c "Add-WindowsCapability -Online -Name OpenSSH.Client"
+powershell -c "Add-WindowsCapability -Online -Name OpenSSH.Server"
+powershell -c "Start-Service sshd"
+powershell -c "Set-Service -Name sshd -StartupType 'Automatic'"
+netsh advfirewall firewall add rule name="SSH" dir=in action=allow protocol=TCP localport=22
 
-:: ------------------------ ::
-
-:: Configurações de rede
-
-echo Aplicando configurações de rede...
+echo Aplicando configurações de firewall...
 netsh advfirewall firewall add rule name="PZ Dedicated Server" dir=in action=allow protocol=UDP localport=16261-16262
 netsh advfirewall firewall add rule name="PZ Dedicated Server" dir=out action=allow protocol=UDP localport=16261-16262
 netsh advfirewall firewall add rule name="Valheim Dedicated Server" dir=in action=allow protocol=UDP localport=2456-2458
@@ -111,6 +152,7 @@ netsh advfirewall firewall add rule name="Valheim Dedicated Server" dir=out acti
 netsh advfirewall firewall add rule name="DST Dedicated Server" dir=in action=allow protocol=UDP localport=10889
 netsh advfirewall firewall add rule name="DST Dedicated Server" dir=out action=allow protocol=UDP localport=10889
 ipconfig /all
+
 echo Configurações de rede aplicadas.
 
 echo.
@@ -155,8 +197,8 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "ColorPrevalence" /t REG_DWORD /d 1 /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
 reg add "HKCU\Control Panel\Desktop" /v "JPEGImportQuality" /t REG_DWORD /d 100 /f
-copy "%RESOURCES_PATH%\wallpaper.png" "%USERPROFILE%\Pictures"
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%USERPROFILE%\Pictures\wallpaper.png" /f
+copy "%RESOURCES_PATH%\wallpaper.png" "%USERPROFILE%\wallpaper.png"
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%USERPROFILE%\wallpaper.png" /f
 rundll32.exe user32.dll,UpdatePerUserSystemParameters
 echo Tema escuro aplicado. Reiniciando o Windows Explorer...
 taskkill /F /IM explorer.exe && start explorer.exe
@@ -177,12 +219,13 @@ echo.
 
 :: Outros recursos
 
-echo Ativando o recurso DirectPlay...
-powershell -Command "if ((Get-WindowsOptionalFeature -Online -FeatureName DirectPlay -ErrorAction SilentlyContinue).State -ne 'Enabled') {dism /online /enable-feature /all /featurename:DirectPlay}"
-echo Ativando o recurso .NET Framework 3.5...
-powershell -Command "if ((Get-WindowsOptionalFeature -Online -FeatureName NetFx3 -ErrorAction SilentlyContinue).State -ne 'Enabled') {dism /online /enable-feature /all /featurename:NetFx3}"
+rem echo Ativando o recurso DirectPlay...
+rem powershell -Command "if ((Get-WindowsOptionalFeature -Online -FeatureName DirectPlay -ErrorAction SilentlyContinue).State -ne 'Enabled') {dism /online /enable-feature /all /featurename:DirectPlay}"
+rem echo Ativando o recurso .NET Framework 3.5...
+rem powershell -Command "if ((Get-WindowsOptionalFeature -Online -FeatureName NetFx3 -ErrorAction SilentlyContinue).State -ne 'Enabled') {dism /online /enable-feature /all /featurename:NetFx3}"
+
 echo Configurando o git...
-%RESOURCES_PATH%\git.bat
+copy "%RESOURCES_PATH%\.gitconfig" "%USERPROFILE%"
 
 echo.
 
