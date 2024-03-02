fd26f98d016b4093ad4b6da76e1ea8ad48c78df1
Reinaldo G. P. Neto <reinaldogpn@outlook.com>
2024-03-02 02:04:01 -0300

    Update win_post_install.bat

--- win_post_install.bat
+++ win_post_install.bat
@@ -1,36 +1,37 @@
 :: -----------------------------------------------------------------------------------------------------
-:: * Description: Batch script for automated installation of applications on Windows 10 and 11.
-:: * Author: reinaldogpn
-:: * Created on: 04/28/2023
+:: * Descrição: Script batch para instalação automatizada de aplicações no Windows 10 e 11. 
+:: * Autor: Reinaldo G. P. Neto                                                             
+:: * Criado em: 28/04/2023                                                                  
 :: -----------------------------------------------------------------------------------------------------
-:: * Changelog:
-::
-::  v1.0 04/28/2023, reinaldogpn:
-::      - Creation of the raw script, polishing will be done in the future =D. The script
-::      performs tests, installs necessary tools for execution, installs
-::      some useful programs and updates the system.
-::  v1.1 04/28/2023, reinaldogpn:
-::      - Correction of the use of the environment variable "!errorlevel!" to work
-::      correctly on Windows 10; now the applications to be installed are defined
-::      inside the file "applist.txt" and no longer as variables within the script.
-::  v1.2 04/28/2023, reinaldogpn:
-::      - Changes in the overall structure of the script, as well as small corrections and adjustments.
-::  v1.3 05/27/2023, reinaldogpn:
-::      - Inclusion of new functions to create two system restore points
-::      and to download tools for customizing Windows elements.
-::  v1.4 05/30/2023, reinaldogpn:
-::      - Refactoring and implementation of the 'curl' command to download the tools.
-::  v1.5 06/01/2023, reinaldogpn:
-::      - Refactoring and error handling in some functions.
-::  v1.6 09/26/2023, reinaldogpn:
-::      - Addition of Windows power settings (suspension time and monitor shutdown).
-::  v2.0 01/04/2024, reinaldogpn:
-::      - Removal of unused packages and tools, code renewal, and new personal settings
-::      for the system.
-::  v2.1 02/17/2024, reinaldogpn:
-::      - Inclusion of additional network and firewall settings; resetting some tests.
-::  v2.2 02/28/2024, reinaldogpn:
-::      - Script translation and minor code fixes.
+:: * Changelog:                                                                             
+::                                                                                          
+::  v1.0 28/04/2023, reinaldogpn:                                                           
+::      - Criação do script bruto, polimentos serão feitos futuramente =D. O script         
+::      realiza testes, instala as ferramentas necessárias para a execução, instala         
+::      alguns programas úteis e faz a atualização do sistema.                              
+::  v1.1 28/04/2023, reinaldogpn:                                                           
+::      - Correção do uso da variável de ambiente "!errorlevel!" para funcionar             
+::      corretamente no Windows 10; agora os aplicativos a serem instalados são definidos   
+::      dentro do arquivo "applist.txt" e não mais em variáveis dentro do script.           
+::  v1.2 28/04/2023, reinaldogpn:                                                           
+::      - Mudanças na estrutura geral do script, além de pequenas correções e ajustes.      
+::  v1.3 27/05/2023, reinaldogpn:                                                           
+::      - Inclusão de novas funções para criação de dois pontos de restauração do sistema   
+::      e para download de ferramentas de personalização de elementos do Windows.           
+::  v1.4 30/05/2023, reinaldogpn:                                                           
+::      - Refatoração e implementação do commando 'curl' para fazer o download das          
+::      ferramentas.            
+::  v1.5 01/06/2023, reinaldogpn:                                                           
+::      - Refatoração e tratamento de erros em algumas funções.
+::  v1.6 26/09/2023, reinaldogpn:                                                           
+::      - Adição de configurações de energia do Windows (tempo de suspensão e desligamento de monitor).
+::  v2.0 04/01/2024, reinaldogpn:
+::      - Remoção de pacotes e ferramentas não utilizadas, renovação do código e novas configurações 
+::      pessoais para o sistema.
+::  v2.1 17/02/2024, reinaldogpn:
+::      - Inclusão de configurações adicionais de rede e firewall; redefinição de alguns testes.
+::  v2.2 17/02/2024, reinaldogpn:
+::      - Pequenas correções e melhorias.
 :: -----------------------------------------------------------------------------------------------------
 
 @echo off
@@ -40,25 +41,27 @@ setlocal EnableDelayedExpansion
 chcp 65001 > nul
 cd /d "%~dp0"
 
-:: ------------ VARS ------------ ::
+:: ------------ VARIÁVEIS ------------ ::
 
-set "APP_list=%~dp0apps.txt"
-set "RES_dir=%~dp0resources"
+set "APP_LIST=%~dp0apps.txt"
+set "RES_DIR=%~dp0resources"
+
 set "OS_name="
 set "OS_version="
 
 :: GitHub info for .gitconfig file:
-set "GIT_user=reinaldogpn"
-set "GIT_email=reinaldogpn@outlook.com"
+set "GIT_USER=reinaldogpn"
+set "GIT_EMAIL=reinaldogpn@outlook.com"
+set "GIT_CONFIG_FILE=%USERPROFILE%\.gitconfig"
 
-:: ------------ TESTS ------------ ::
+:: ------------ TESTES ------------ ::
 
-:: Running as admin?
+:: Executando como admin?
 
-echo Checking administrator privileges...
+echo Verificando privilégios de administrador...
 net session >nul 2>&1
 if !errorlevel! neq 0 (
-    echo This script needs to be run with administrator privileges. The script will be terminated.
+    echo Este script precisa ser executado com privilégios de administrador. O script será encerrado.
     goto :end
 )
 
@@ -66,12 +69,12 @@ echo.
 
 :: ------------------------ ::
 
-:: Connected to the internet?
+:: Conectado à internet?
 
-echo Checking internet connection...
+echo Verificando conexão com a internet...
 ping -n 1 8.8.8.8 >nul 2>&1
 if !errorlevel! neq 0 (
-    echo No internet connection. The script will be terminated.
+    echo Não há conexão com a internet. O script será encerrado.
     goto :end
 )
 
@@ -79,9 +82,9 @@ echo.
 
 :: ------------------------ ::
 
-:: Windows version?
+:: Versão do Windows?
 
-for /f "tokens=*" %%a in ('systeminfo ^| findstr /B /C:"OS Name"') do (
+for /f "tokens=*" %%a in ('systeminfo ^| findstr /B /C:"Nome do sistema operacional"') do (
     set "count=0"
     for %%b in (%%a) do (
         set /a count+=1
@@ -93,14 +96,14 @@ for /f "tokens=*" %%a in ('systeminfo ^| findstr /B /C:"OS Name"') do (
 
 set "OS_name=!OS_name:~1!"
 
-for /f "tokens=7" %%c in ('systeminfo ^| findstr /B /C:"OS Version"') do (
+for /f "tokens=7" %%c in ('systeminfo ^| findstr /B /C:"Nome do sistema operacional"') do (
     set "OS_version=%%c"
 )
 
-echo Identified operating system: %OS_name%
+echo Sistema operacional identificado: %OS_name%
 
 if %OS_version% lt 10 (
-    echo Windows version not supported. The script will be terminated.
+    echo Versão do Windows não suportada. O script será encerrado.
     goto :end
 )
 
@@ -108,59 +111,59 @@ echo.
 
 :: ------------------------ ::
 
-:: Winget installed?
+:: Winget instalado?
 
-echo Checking winget installation...
+echo Verificando a instalação do winget...
 where winget >nul 2>&1 || (
-    echo Installing winget and its dependencies...
-    powershell -Command "Add-AppxPackage -Path '%RES_dir%\winget\Microsoft.UI.Xaml_7.2208.15002.0_X64_msix_en-US.msix'"
-    powershell -Command "Add-AppxPackage -Path '%RES_dir%\winget\Microsoft.VC.2015.UWP.DRP_14.0.30704.0_X64_msix_en-US.msix'"
+    echo Instalando o winget e suas dependências...
+    powershell -Command "Add-AppxPackage -Path '%RES_DIR%\winget\Microsoft.UI.Xaml_7.2208.15002.0_X64_msix_en-US.msix'"
+    powershell -Command "Add-AppxPackage -Path '%RES_DIR%\winget\Microsoft.VC.2015.UWP.DRP_14.0.30704.0_X64_msix_en-US.msix'"
     powershell -Command "Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile winget.msixbundle; .\winget.msi"
     echo y | winget list >nul 2>&1
     if !errorlevel! neq 0 (
-        echo Failed to install winget. The script will be terminated.
+        echo Falha ao tentar instalar o winget. O script será encerrado.
         goto :end
     )
 )
 
-echo Updating winget...
+echo Atualizando o winget...
 winget upgrade Microsoft.AppInstaller --accept-package-agreements --accept-source-agreements --disable-interactivity --silent >nul 2>&1
 if !errorlevel! equ 0 (
-    echo Winget is properly installed and updated.
+    echo Winget está devidamente instalado e atualizado.
 ) else (
-    echo Failed to update winget.
+    echo Falha ao tentar atualizar o winget.
 )
 
 echo.
 
-:: ------------ FUNCTIONS ------------ ::
+:: ------------ FUNÇÕES ------------ ::
 
-:: System restore point 1
+:: Ponto de restauração 1
 
-echo Creating system restore point...
+echo Criando ponto de restauração do sistema...
 powershell -Command "Enable-ComputerRestore -Drive 'C:\'"
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v SystemRestorePointCreationFrequency /t REG_DWORD /d 1 /f
-powershell -Command "Checkpoint-Computer -Description 'Pre-Execution of Windows Post Install Script'"
-echo System restore point created.
+powershell -Command "Checkpoint-Computer -Description 'Pré Execução do Script Windows Post Install'"
+echo Ponto de restauração do sistema criado.
 
 echo.
 
 :: ------------------------ ::
 
-:: Network settings and services
+:: Configurações e serviços de rede
 
-echo Enabling FTP service...
+echo Habilitando serviço FTP...
 powershell -Command "dism /online /enable-feature /featurename:IIS-WebServerRole /all"
 powershell -Command "dism /online /enable-feature /featurename:IIS-WebServer /all"
 powershell -Command "dism /online /enable-feature /featurename:IIS-FTPServer /all"
 
-echo Enabling SSH service...
+echo Habilitando serviço SSH...
 powershell -Command "Add-WindowsCapability -Online -Name OpenSSH.Client"
 powershell -Command "Add-WindowsCapability -Online -Name OpenSSH.Server"
 powershell -Command "Start-Service sshd"
 powershell -Command "Set-Service -Name sshd -StartupType 'Automatic'"
 
-echo Creating firewall rules...
+echo Criando regras de firewall...
 netsh advfirewall firewall add rule name="FTP" dir=in action=allow protocol=TCP localport=21
 netsh advfirewall firewall add rule name="SSH" dir=in action=allow protocol=TCP localport=22
 netsh advfirewall firewall add rule name="PZ Dedicated Server" dir=in action=allow protocol=UDP localport=16261-16262
@@ -171,65 +174,65 @@ netsh advfirewall firewall add rule name="DST Dedicated Server" dir=in action=al
 netsh advfirewall firewall add rule name="DST Dedicated Server" dir=out action=allow protocol=UDP localport=10889
 ipconfig /all
 
-echo Network settings applied.
+echo Configurações de rede aplicadas.
 
 echo.
 
 :: ------------------------ ::
 
-:: Package installation
+:: Instalação de pacotes
 
-echo To add or remove packages from the script, modify the file "%APP_list%"
-echo To find out the ID of the desired application, use "winget search <appname>" in the terminal.
-if not exist %APP_list% (
-    echo Package list not found: "%APP_list%"
+echo Para acrescentar ou remover pacotes ao script, modifique o arquivo "%APP_LIST%"
+echo Para descobrir o ID da aplicação desejada, use "winget search <nomedoapp>" no terminal.
+if not exist %APP_LIST% (
+    echo Lista de pacotes não encontrada: "%APP_LIST%"
     goto :end
 )
 
 set "count=0"
-for /f "usebackq delims=" %%a in (%APP_list%) do (
+for /f "usebackq delims=" %%a in (%APP_LIST%) do (
     set "app_name=%%a"
     winget list !app_name! > nul 2>&1
     if !errorlevel! equ 0 (
-        echo !app_name! is already installed...
+        echo !app_name! já está instalado...
     ) else (
-        echo Installing !app_name!...
+        echo Instalando !app_name!...
         winget install --id !app_name! --accept-package-agreements --accept-source-agreements --disable-interactivity --silent
         if !errorlevel! equ 0 set /a count+=1
     )
 )
-echo %count% packages were successfully installed.
+echo %count% pacotes foram instalados com sucesso.
 
-echo Installing DriverBooster...
-%RES_dir%\driver_booster_setup.exe /verysilent /suppressmsgboxes
+echo Instalando DriverBooster...
+%RES_DIR%\driver_booster_setup.exe /verysilent /supressmsgboxes
 
 echo.
 
 :: ------------------------ ::
 
-:: System customizations
+:: Customizações do sistema
 
-:: Taskbar, dark theme, and wallpaper
-echo Customizing Windows
+:: Taskbar, tema escuro e wallpaper
+echo Customizando o Windows
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Feeds" /v "ShellFeedsTaskbarViewMode" /t REG_DWORD /d 2 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v "SearchBoxTaskbarMode" /t REG_DWORD /d 1 /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "ColorPrevalence" /t REG_DWORD /d 1 /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
 reg add "HKCU\Control Panel\Desktop" /v "JPEGImportQuality" /t REG_DWORD /d 100 /f
-copy "%RES_dir%\wallpaper.png" "%USERPROFILE%\wallpaper.png"
+copy "%RES_DIR%\wallpaper.png" "%USERPROFILE%\wallpaper.png"
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%USERPROFILE%\wallpaper.png" /f
 rundll32.exe user32.dll,UpdatePerUserSystemParameters
-echo Dark theme applied. Restarting Windows Explorer...
+echo Tema escuro aplicado. Reiniciando o Windows Explorer...
 taskkill /F /IM explorer.exe && start explorer.exe
 
 echo.
 
 :: ------------------------ ::
 
-:: Power settings
+:: Configurações de energia
 
-echo Changing Windows power settings...
+echo Alterando configurações de energia do Windows...
 powercfg /change standby-timeout-ac 0
 powercfg /change standby-timeout-dc 0
 
@@ -237,38 +240,39 @@ echo.
 
 :: ------------------------ ::
 
-:: Other resources
+:: Outros recursos
 
-rem echo Activating DirectPlay feature...
+rem echo Ativando o recurso DirectPlay...
 rem powershell -Command "if ((Get-WindowsOptionalFeature -Online -FeatureName DirectPlay -ErrorAction SilentlyContinue).State -ne 'Enabled') {dism /online /enable-feature /all /featurename:DirectPlay}"
-rem echo Activating .NET Framework 3.5 feature...
+rem echo Ativando o recurso .NET Framework 3.5...
 rem powershell -Command "if ((Get-WindowsOptionalFeature -Online -FeatureName NetFx3 -ErrorAction SilentlyContinue).State -ne 'Enabled') {dism /online /enable-feature /all /featurename:NetFx3}"
 
-echo Configuring git...
+echo Configurando o git...
 
-rem copy "%RES_dir%\gitconfig.txt" "%USERPROFILE%\.gitconfig"
+rem copy "%RES_DIR%\gitconfig.txt" "%USERPROFILE%\.gitconfig"
 
-echo [user] >> %USERPROFILE%\.gitconfig
-echo     name = %GITUSER% >> %USERPROFILE%\.gitconfig
-echo     email = %GITEMAIL% >> %USERPROFILE%\.gitconfig
+echo [user] >> %GIT_CONFIG_FILE%
+echo     name = %GIT_USER% >> %GIT_CONFIG_FILE%
+echo     email = %GIT_EMAIL% >> %GIT_CONFIG_FILE%
 
 echo.
 
 :: ------------------------ ::
 
-:: System restore point 2
+:: Ponto de restauração 2
 
-echo Creating system restore point...
-powershell -Command "Checkpoint-Computer -Description 'Post-Execution of Windows Post Install Script'"
+echo Criando ponto de restauração do sistema...
+powershell -Command "Checkpoint-Computer -Description 'Pós Execução do Script Windows Post Install'"
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" /v SystemRestorePointCreationFrequency /f
-echo System restore point created.
+echo Ponto de restauração do sistema criado.
 
 echo.
 
-:: ------------ END ------------ ::
+:: ------------ FIM ------------ ::
 
 :end
-echo End of script!
+echo Fim do script!
 endlocal
 pause
+color
 exit /b
